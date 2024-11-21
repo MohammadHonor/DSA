06eb0bd89b82634621ae552a7ebcc82dd3161e47
Sharuf <ali@gmail.com>
2024-11-22 03:10:02 +0530

    by recursion method

--- DP/src/App.java
+++ DP/src/App.java
@@ -1,5 +1,8 @@
+import DP.dpQuestions.LongestCommonSequence;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        LongestCommonSequence ls = new LongestCommonSequence() ;
+        System.out.println(ls.solve("abcde","ace", 0, 0));
     }
 }
