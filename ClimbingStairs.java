274c9ae0eb39d07df5893e56ac995b4d43bf82a3
Sharuf <ali@gmail.com>
2024-12-01 10:24:29 +0530

    By Memoization

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,7 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ClimbingStairs {
-      static int solve(int n ){
+      // By recursion 
+      // static int solve(int n ){
+
+      //       if(n == 0){
+      //             return 1;
+      //       }
+      //       if(n <0){
+      //             return 0;
+      //       }
+      //       int totalWay=0;
+      //       totalWay += solve(n-1);
+      //       totalWay += solve(n-2);
+
+      //       return totalWay;
+      // }
+
+      // By dp 
+      static int solveMemo(int n ,int dp[]){
 
             if(n == 0){
                   return 1;
@@ -9,17 +27,23 @@ public class ClimbingStairs {
             if(n <0){
                   return 0;
             }
+            if(dp[n]!=-1){
+                  return dp[n];
+            }
             int totalWay=0;
-            totalWay += solve(n-1);
-            totalWay += solve(n-2);
+            totalWay+= solveMemo(n-1,dp);
+            totalWay+= solveMemo(n-2,dp);
+            dp[n]=totalWay;
 
-            return totalWay;
+            return dp[n];
       }
       public static void main(String[] args) {
             System.out.println("Enter steps to reach the top : ");
             Scanner sc = new Scanner(System.in);
             int n  = sc.nextInt();
-            System.out.print(solve(n));
+            int []dp = new int[n+1];
+            Arrays.fill(dp,-1);
+            System.out.print(solveMemo(n, dp));
             sc.close();
       }
 }
